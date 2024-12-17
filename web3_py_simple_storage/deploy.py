300df99e1bfbcda74d607c7ca46f54b444abee0c
omeev <omee.verma22@gmail.com>
2024-12-17 21:20:47 +0530

    Blockchain-wallet-with-meta-mask-and-Solidity file added

---
+++ web3_py_simple_storage/deploy.py
@@ -0,0 +1,21 @@
+from solcx import compile_standard
+import json
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+    #print(simple_storage_file)
+# Compile our Solidity
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file }},
+        "settings": {
+            "outputSelection": {
+                "*" : { "*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+    sole_version="0.8.10",
+)
+with open ("compiled_code.json","w") as file:
+    json.dump(compiled_sol, file)
